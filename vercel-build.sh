bb00aff94a4fe54c4b34fcf46a0616982c6a2f34
hejiehejiehejiehejie <167274887+hejiehejiehejiehejie@users.noreply.github.com>
2026-04-20 04:24:56 +0800

    new .sh

---
+++ vercel-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "🚀 开始下载 Linux 版 Pandoc..."
+curl -L https://github.com/jgm/pandoc/releases/download/3.1.1/pandoc-3.1.1-linux-amd64.tar.gz | tar xz
+
+echo "🔗 将 Pandoc 注入 Vercel 环境变量..."
+export PATH=$PWD/pandoc-3.1.1/bin:$PATH
+
+echo "✅ Pandoc 安装完毕，版本信息如下："
+pandoc -v
+
+echo "🔨 开始生成 Hexo 博客静态文件..."
+hexo g
+
+echo "🔍 开始自动向云端推送 Algolia 搜索索引..."
+hexo algolia
