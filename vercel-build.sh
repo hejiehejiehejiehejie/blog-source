68c821f3a4c980241aa63b09556644f4ad5f281e
hejiehejiehejiehejie <167274887+hejiehejiehejiehejie@users.noreply.github.com>
2026-04-20 05:05:48 +0800

    vercel-apikey

--- vercel-build.sh
+++ vercel-build.sh
@@ -9,6 +9,14 @@ export PATH=$PWD/pandoc-3.1.1/bin:$PATH
 echo "✅ Pandoc 安装完毕，版本信息如下："
 pandoc -v
 
+echo "🕵️ 动态注入 Algolia 密钥到配置文件..."
+if [ -z "$ALGOLIA_ADMIN_API_KEY" ]; then
+  echo "❌ 警告：Vercel 环境变量中没有找到 ALGOLIA_ADMIN_API_KEY，注入失败！请检查 Vercel 后台！"
+else
+  sed -i "s/VERCEL_ALGOLIA_KEY/$ALGOLIA_ADMIN_API_KEY/g" _config.yml
+  echo "✅ 密钥动态注入成功！"
+fi
+
 echo "🔨 开始生成 Hexo 博客静态文件..."
 hexo g
 
